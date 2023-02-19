11c5ecf2c796f7e3236f3117185c384e7df4affe
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2023-02-19 22:27:11 +0600

    Database connection successful

--- index.js
+++ index.js
@@ -6,20 +6,32 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bravo.9rdyqfd.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.use(cors());
 app.use(express.json());
 
 
-function run(){
-    try {
-        const AccountsCollection = client.db('Bravo-Bank').collection('Accounts');
 
-        
+
+
+function run() {
+    try {
+        const accountsCollection = client.db("BravoBank").collection("accounts");
+        const accountTypesCollection = client.db("BravoBank").collection("accountTypes");
+
+        app.get('/accounts', async (req, res) => {
+            const query = {};
+            const result = await accountsCollection.find(query).toArray();
+            res.send(result);
+        })
+
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 run();

5626f4f800757d1e8aeab519b8a422ee652669e8
Md Galib Hossain <mdgalib23@gmail.com>
2023-02-22 13:19:06 +0600

    Backend card types added

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
 function run() {
-    try {
-        const accountsCollection = client.db("BravoBank").collection("accounts");
-        const accountTypesCollection = client.db("BravoBank").collection("accountTypes");
+  try {
+    const accountsCollection = client.db("BravoBank").collection("accounts");
+    const accountTypesCollection = client
+      .db("BravoBank")
+      .collection("accountTypes");
+    const cardsCollection = client.db("BravoBank").collection("cards");
+    const cardTypesCollection = client.db("BravoBank").collection("cardTypes");
 
+    app.get("/accountsTypes", async (req, res) => {
+      const query = {};
+      const result = await accountTypesCollection.find(query).toArray();
+      res.send(result);
+    });
 
-        app.get('/accountsTypes', async (req, res) => {
-            const query = {};
-            const result = await accountTypesCollection.find(query).toArray();
-            res.send(result);
-        })
+    app.get("/accounts/:accountType", async (req, res) => {
+      const accountType = req.params.accountType;
+      const query = { accountType: accountType };
+      const result = await accountsCollection.findOne(query);
+      res.send(result);
+    });
 
-        app.get('/accounts/:accountType', async (req, res) => {
-                const accountType = req.params.accountType;
-                const query = { accountType: accountType }
-                const result = await accountsCollection.findOne(query)
-                res.send(result);
-        })
-
-    } catch (error) {
-        console.log(error);
-    }
+    app.get("/cardsTypes", async (req, res) => {
+      const query = {};
+      const result = await cardTypesCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/cards/:cardType", async (req, res) => {
+      const cardType = req.params.cardType;
+      const query = { cardType: cardType };
+      const result = await cardsCollection.findOne(query);
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 run();
 
-app.get('/', (req, res) => {
-    res.send('Bravo-6 is running...')
-})
+app.get("/", (req, res) => {
+  res.send("Bravo-6 is running...");
+});
 
 app.listen(port, async (req, res) => {
-    console.log(`Bravo-Bank is running on ${port}`);
+  console.log(`Bravo-Bank is running on ${port}`);
 });
